88aa24f299a86ae03b375ea2b4f859653d177f29
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-04-15 04:09:55 -0300

    Replaced Input Mask With React Input Mask Next
    - Uninstalled "react-input-mask" And Its Type Declarations And Installed "react-input-mask-next";

--- src/Pages/Checkout/index.tsx
+++ src/Pages/Checkout/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
-import InputMask from 'react-input-mask'
+import InputMask from 'react-input-mask-next'
 
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
